358d1062d7ed37c7204e7f14d764f499ad13c182
Mohammed Noori <kmanoorib@gmail.com>
2019-01-02 06:30:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
+    session[:user_id] = @user.id
     
     redirect '/users/home'
   end
@@ -40,7 +40,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-    @user = User.find(session[:id])
+    @user = User.find(session[:user_id])
     
     erb :'/users/home'
   end
